37d0e1458cb5c64b1c78a59b335ca316613cc7f3
Ridhuan Rangga Kusuma <ridhuankeren@gmail.com>
2025-08-30 02:32:16 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,12 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL, // FE URL
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, // FE URL
+    credentials: true,
+  })
+);
 
 // reverse proxy trust (agar secure cookie & sameSite:none bekerja di host HTTPS)
 app.set("trust proxy", 1);
@@ -26,14 +27,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/debug", (req, res) => { 
+app.get("/debug", (req, res) => {
   res.json({
     message: "Debug API working!",
     timestamp: new Date(),
-    headers: req.headers
+    headers: req.headers,
   });
 });
-
 
 // ===== API routes =====
 app.use("/api/auth", authRoutes);
@@ -45,6 +45,12 @@ app.use(express.static(path.join(__dirname, "frontend/dist")));
 // Layani semua route non-API ke index.html (SPA)
 app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000; // bisa dari env atau default 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 connectDb();
